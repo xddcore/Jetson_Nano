45388c48991f9ed8a6eea2a9db28a6fe21564613
xdd <1034029664@qq.com>
2020-01-18 16:57:27 +0800

    "update example"

---
+++ example/Regression/Linear Regression/Linear Regression.py
@@ -0,0 +1,65 @@
+import tensorflow as tf
+
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+
+x_data = np.linspace(-0.5, 0.5, 200)[:, np.newaxis]
+
+noise = np.random.normal(0, 0.02, x_data.shape)
+
+y_data = np.square(x_data) + noise
+
+
+x = tf.placeholder(tf.float32, [None, 1])
+
+y = tf.placeholder(tf.float32, [None, 1])
+
+
+Weights_L1 = tf.Variable(tf.random.normal([1, 10]))
+
+Biases_L1 = tf.Variable(tf.zeros([1, 10]))
+
+Wx_plus_b_L1 = tf.matmul(x, Weights_L1) + Biases_L1
+
+L1 = tf.nn.tanh(Wx_plus_b_L1)
+
+
+
+Weights_L2 = tf.Variable(tf.random.normal([10, 1]))
+
+Biases_L2 = tf.Variable(tf.zeros([1, 1]))
+
+Wx_plus_b_L2 = tf.matmul(L1, Weights_L2) + Biases_L2
+
+pred = tf.nn.tanh(Wx_plus_b_L2)
+
+
+
+loss = tf.reduce_mean(tf.square(y - pred))
+
+
+
+train = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
+
+with tf.Session() as sess:
+
+ sess.run(tf.global_variables_initializer())
+
+ for i in range(2000):
+
+    sess.run(train, feed_dict={x: x_data, y: y_data})
+
+    print("The {0}:,loss = {1}".format(i, sess.run(loss,feed_dict={x: x_data, y: y_data})))
+
+ pred_vaule = sess.run(pred, feed_dict={x: x_data})
+
+ plt.figure()
+
+ plt.scatter(x_data, y_data)
+
+ plt.plot(x_data, pred_vaule, 'r-', lw=5)
+
+ plt.show()
